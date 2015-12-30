70bbb7b05983c2f52a58ccad2e76960a28b9a6cf
Quantum <quantum2048@gmail.com>
2015-12-30 17:12:22 -0500

    Use fancy dictionary to allow executors to be loaded on demand

--- executors/__init__.py
+++ executors/__init__.py
@@ -1,49 +1,63 @@
 import os
 import re
+import traceback
+
 from judgeenv import env, only_executors, exclude_executors
 
-_reexecutor = re.compile('([A-Z0-9]+)\.py$')
-__executors = set(i.group(1) for i in map(_reexecutor.match,
-                                       os.listdir(os.path.dirname(__file__)))
-                  if i is not None)
+reexecutor = re.compile('([A-Z0-9]+)\.py$')
+available = set(i.group(1) for i in map(reexecutor.match,
+                                        os.listdir(os.path.dirname(__file__)))
+                if i is not None)
 if only_executors:
-    __executors &= only_executors
+    available &= only_executors
 if exclude_executors:
-    __executors -= exclude_executors
-__executors = sorted(__executors)
+    available -= exclude_executors
+available = sorted(available)
 
-executors = {}
+package_path = __name__.split('.')[1:]
 
-def __load(to_load):
-    import traceback
 
-    path = __name__.split('.')[1:]
+def load_module(executor):
+    try:
+        module = __import__('%s.%s' % (__name__, executor))
+    except ImportError as e:
+        if e.message not in ('No module named _cptbox',
+                             'No module named msvcrt',
+                             'No module named _wbox'):
+            traceback.print_exc()
+        return None
+    for part in package_path:
+        module = getattr(module, part)
+    return getattr(module, executor)
 
-    def __load_module(executor):
-        try:
-            module = __import__('%s.%s' % (__name__, executor))
-        except ImportError as e:
-            if e.message not in ('No module named _cptbox',
-                                 'No module named msvcrt',
-                                 'No module named _wbox'):
-                traceback.print_exc()
-            return None
-        for part in path:
-            module = getattr(module, part)
-        return getattr(module, executor)
 
-    for name in to_load:
-        executor = __load_module(name)
+class ExecutorLoader(dict):
+    def __init__(self, available):
+        super(ExecutorLoader, self).__init__()
+        self._available = available
+
+    def _load(self, name):
+        executor = load_module(name)
         if executor is None:
-            continue
+            return None
         if hasattr(executor, 'initialize') and not executor.initialize(sandbox=env.get('selftest_sandboxing', True)):
-            continue
+            return None
         if hasattr(executor, 'aliases'):
             for alias in executor.aliases():
-                executors[alias] = executor
+                self[alias] = executor
         else:
-            executors[name] = executor
+            self[name] = executor
+        return executor
+
+    def __missing__(self, key):
+        executor = self._load(key)
+        if executor is None:
+            raise KeyError()
+        return executor
+
+    def load_all(self):
+        for name in self._available:
+            self._load(name)
 
 
-__load(__executors)
-del __executors, __load, env, only_executors, exclude_executors
+executors = ExecutorLoader(available)
