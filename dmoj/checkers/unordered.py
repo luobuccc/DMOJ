79988454d8d17c5eaed02f60f51aad54fb11a39c
BinAccel <BinAccel@users.noreply.github.com>
2017-03-05 14:57:33 -0500

    Don't need this

--- dmoj/checkers/unordered.py
+++
@@ -1,4 +0,0 @@
-def check(process_output, judge_output, **kwargs):
-    process_tokens = process_output.split()
-    judge_tokens = judge_output.split()
-    return len(process_tokens) == len(judge_tokens) and sorted(process_tokens) == sorted(judge_tokens)
